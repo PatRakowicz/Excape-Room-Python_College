d29aba4bd8c930353c71af0a70675d1badfebf66
Patrick Rakowicz <lividsubset3@gmail.com>
2021-12-08 19:43:51 -0700

    organization

--- Stage_One.py
+++ Stage_One.py
@@ -12,6 +12,22 @@ def clicked(*args):
     print('box Clicked')
 
 
+def safe_click(*args):
+    global entry
+    x = Toplevel()
+    canvas_two = Canvas(x, width=500, height=500)
+    entry = Entry(x)
+    entry.pack()
+    button = Button(x, text='Enter', command=fx)
+    button.pack()
+
+
+# safe click action 
+def fx():
+    key = entry.get()
+    print(key)
+
+
 # Everything goes in here
 def Main(root):
     global canvas
@@ -29,25 +45,10 @@ def Main(root):
 
     # Packing
     canvas.pack(fill="both", expand=True)
-    
-    # safe click action 
-    def fx():
-        key = entry.get()
-        print(key)
 
-    def safe_click(*args):
-        global entry
-        x = Bottomlevel()
-        canvas_two = Canvas(x, width=500, height=500)
-        entry = Entry(x)
-        entry.pack()
-        button = Button(x, text='Enter', command=fx)
-        button.pack()
-    
-    #safe
+    # safe
     safe = canvas.create_rectangle(0, 215, 50, 270, fill='gray', tag='safe')
     canvas.create_text(25, 200, text="Safe", fill="black", font=('Helvetica 15 bold'))
     canvas.tag_bind('safe', '<Button-1>', safe_click)
-
 
     newWindow.mainloop()
