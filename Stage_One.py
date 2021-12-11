e0274fe5e3892c20a5085c783be2faca44685c91
Patrick Rakowicz <lividsubset3@gmail.com>
2021-12-10 21:29:23 -0700

    finished most of the functions | Needs random crates now

--- Stage_One.py
+++ Stage_One.py
@@ -3,38 +3,45 @@ from tkinter import *
 from tkinter import messagebox
 import time
 import os
+import random
 
 # start timer
 start_time = time.time()
+
+# Random 4 Digit Number
+fDigitNum = random.randint(1000, 9999)
 
 
 def exitStage(*args):
     global code_entry
     x = Toplevel()
     code_entry = Entry(x)
+    x.geometry('270x50')
+    x.resizable(False, False)
+    x.title('Pass Code')
+    button = Button(x, text='Enter', command=exitDoor)
+
     code_entry.pack()
-    button = Button(x, text='Enter', command=lx)
     button.pack()
-    # I think we need to move this stuff below outside of this function
-    newWindow = Toplevel()
-    newWindow.geometry('100x100')
-    newWindow.resizable(False, False)
-    canvas = Canvas(newWindow)
-    canvas.create_text(50, 50, text='Game Complete!', fill='black', font='Helvetica 15 bold')
 
 
+# Click on safe and have user input keycard.txt for exit door passcode
 def safeClick(*args):
     global entry
     x = Toplevel()
     entry = Entry(x)
-    entry.pack()
+    keycardRequired = Label(x, text='Keycard Required')
     button = Button(x, text='Enter', command=keycardCheck)
+
+    keycardRequired.pack()
+    entry.pack()
     button.pack()
 
 
 # make keycard.txt with content for safe
 def crateClick(*args):
     print('crate click')
+    # check if file exists, if not creates file with parameters keycard.ex
     if not os.path.exists("keycard.txt"):
         key_file = open('keycard.txt', 'w+')
         key_file.write("keycard.ex")
@@ -46,26 +53,30 @@ def crateClick(*args):
 # safe click action
 def keycardCheck():
     # checks for valid 'keycard' input if correct displays code if not clears entry box and displays error
-    key = entry.get()
-    if key == 'keycard.ex':
-        messagebox.showinfo('Code', '1776')
-    else:
-        entry.delete('0', 'end')
-        messagebox.showwarning('Error', 'Invalid input')
+    print(fDigitNum)
+    with open(entry.get()) as f:
+        line = f.readline()
+        if line == 'keycard.ex':
+            messagebox.showinfo('Exit #', str(fDigitNum))
+        else:
+            entry.delete('0', 'end')
+            messagebox.showwarning('Error', 'Invalid input or Keycard Missing')
 
 
 # exit door button action
-def lx():
+def exitDoor():
     exit_code = code_entry.get()
-    if exit_code == '1776':
+    print(fDigitNum)
+    if exit_code == str(fDigitNum):
         # end timer + calculate final time
         end_time = time.time()
         final_time = end_time - start_time
         # not sure if a variable can be included in a message box
-        messagebox.showinfo('Congratulations', 'you completed the escape room in' + str(final_time))
+        messagebox.showinfo('Congratulations!', 'You completed the escape room in ' + str(final_time))
+        messagebox.showinfo('Exit Game', 'Use exit box in Main Menu to exit game')
     else:
         code_entry.delete('0', 'end')
-        messagebox.showwarning('Error', 'Invalid input')
+        messagebox.showwarning('Error', 'Invalid input or Wrong Password')
 
 
 # Everything goes in here
@@ -92,6 +103,10 @@ def Main(root):
     canvas.create_rectangle(400, 425, 495, 475, fill='blue', tag='crate')
     canvas.create_text(445, 415, text='Crate', fill='black', font='Helvetica 15 bold')
     canvas.tag_bind('crate', '<Button-1>', crateClick)
+
+    # Random Crate
+    # Random Crate 2
+    # Random Crate 3
 
     # Packing
     canvas.pack(fill="both", expand=True)
